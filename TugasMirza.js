22ceafdebd5e774afa179638142255a52eccec44
mRzz129 <114788470+mRzz129@users.noreply.github.com>
2023-09-27 19:14:57 +0700

    Add files via upload

---
+++ TugasMirza.js
@@ -0,0 +1,21 @@
+// Data mahasiswa
+const nama = "Moh. Mirza yusuf humaedy";
+const nim = "20220040260";
+const jurusan = "Teknik Informatika";
+const ipk = 4;
+
+console.log("Data Mahasiswa:");
+console.log("Nama: " + nama);
+console.log("NIM: " + nim);
+console.log("Jurusan: " + jurusan);
+console.log("Ipk: " + ipk);
+// Operasi Aritmatika
+const angka1 = 10;
+const angka2 = 5;
+
+console.log("\nOperasi Aritmatika:");
+console.log("Penjumlahan: " + (angka1 + angka2));
+console.log("Pengurangan: " + (angka1 - angka2));
+console.log("Perkalian: " + (angka1 * angka2));
+console.log("Pembagian: " + (angka1 / angka2));
+console.log("Modulus (Sisa Bagi): " + (angka1 % angka2));
